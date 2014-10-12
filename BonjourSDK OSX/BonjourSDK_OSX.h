7cc0d1ea8459b36a2dc98c9b6fc6c6efd4738c28
Void Main <voidmain1313113@gmail.com>
2014-10-12 11:23:04 +0800

    update bonjour sdk

--- BonjourSDK OSX/BonjourSDK_OSX.h
+++ BonjourSDK OSX/BonjourSDK_OSX.h
@@ -14,6 +14,5 @@ FOUNDATION_EXPORT double BonjourSDK_OSXVersionNumber;
 //! Project version string for BonjourSDK OSX.
 FOUNDATION_EXPORT const unsigned char BonjourSDK_OSXVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <BonjourSDK_OSX/PublicHeader.h>
-
+#import <BonjourSDK/BSBonjourManager.h>
 
