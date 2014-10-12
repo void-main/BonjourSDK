d825b29fd38b6dda0b3ba5e4e6ce0c9d2c2796bb
Void Main <voidmain1313113@gmail.com>
2014-10-12 13:58:45 +0800

    remove useless schemes, remove irrelavant includes in main header file

--- BonjourSDK/BonjourSDK.h
+++ BonjourSDK/BonjourSDK.h
@@ -6,12 +6,4 @@
 //  Copyright (c) 2014年 Peng Sun. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
-
-//! Project version number for BonjourSDK.
-FOUNDATION_EXPORT double BonjourSDKVersionNumber;
-
-//! Project version string for BonjourSDK.
-FOUNDATION_EXPORT const unsigned char BonjourSDKVersionString[];
-
 #import <BonjourSDK/BSBonjourManager.h>
